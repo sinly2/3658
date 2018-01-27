0a4aa32367d1d89fcb28886382d82113ecba4b4b
15380794703@163.com <www83101113>
2018-01-27 12:48:06 +0800

    加入红金宝提现，修改返回code
    Signed-off-by: 15380794703@163.com <www83101113>

--- base.py
+++ base.py
@@ -7,11 +7,12 @@ Created on Jan 23, 2018
 import requests
 import redis
 
+
 class Session(object):
 
-    def __init__(self,username,password,trade_password):
+    def __init__(self, username, password, trade_password):
         self.session = requests.Session()
-        self.r_connector = redis.Redis(host="127.0.0.1",port=6379,db=0)
+        self.r_connector = redis.Redis(host="127.0.0.1", port=6379, db=0)
         self.username = username
         self.password = password
         self.trade_password = trade_password
@@ -19,28 +20,29 @@ class Session(object):
         self.flag = False
         self.keys = []
         self.key = ""
-        self.login_url = "https://www.3658mall.com/user.php?act=act_login_new"
-        self.withdraw_url = "https://www.3658mall.com/user.php?act=act_account"
+        self.withdraw_url = "https://www.3658mall.com/user.php"
         self.user_agent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.221 " \
                           "Safari/537.36 SE 2.X MetaSr 1.0"
-        self.headers = {"user-agent":self.user_agent,"accept-encoding":"gzip,deflate","accept-language":"zh-CN,zh;q=0.8",
-                        "origin":"https://www.3658mall.com","content-type":"application/x-www-form-urlencoded","referer":
-                        "https://www.3658mall.com/member.html","Connection":"keep-alive"}
+        self.headers = {"user-agent": self.user_agent, "accept-encoding": "gzip,deflate",
+                        "accept-language": "zh-CN,zh;q=0.8",
+                        "origin": "https://www.3658mall.com", "content-type": "application/x-www-form-urlencoded",
+                        "referer":
+                            "https://www.3658mall.com/member.html", "Connection": "keep-alive"}
 
     def close_session(self):
         self.session.close()
 
     def store_cookie(self):
         cookies = requests.utils.dict_from_cookiejar(self.session.cookies)
-        self.r_connector.set(self.username+":"+str(hash(str(cookies))),cookies,1800)
+        self.r_connector.set(self.username + ":" + str(hash(str(cookies))), cookies, 1800)
 
     def set_cookies(self):
-        self.keys = self.r_connector.keys(self.username+"*")
+        self.keys = self.r_connector.keys(self.username + "*")
         if self.keys:
             self.key = self.keys.pop()
             cookies = self.r_connector.get(self.key)
             cookies = eval(cookies)
-            cookies = requests.utils.cookiejar_from_dict(cookies,cookiejar=None,overwrite=True)
+            cookies = requests.utils.cookiejar_from_dict(cookies, cookiejar=None, overwrite=True)
             self.session.cookies = cookies
             return True
         else:
@@ -53,29 +55,29 @@ class Session(object):
     def cookie_is_available(self):
         pass
 
-    def login(self):
+    def login(self,act="act_login_new"):
         self.clear_cookies()
-        params = {"username":self.username,"password":self.password,"msgCode":""}
+        params = {"username": self.username, "password": self.password, "msgCode": "","act":act}
         try:
-            result = self.session.post(self.login_url, data=params, headers=self.headers, timeout=self.timeout)
-            #print result.text
-            #print result.headers
-            #print result.status_code
+            result = self.session.post(self.withdraw_url, data=params, headers=self.headers, timeout=self.timeout)
+            # print result.text
+            # print result.headers
+            # print result.status_code
             if result.status_code == 200 and result.json()["status"] is True:
                 self.flag = True
                 return True
             else:
                 return False
-        except Exception,e:
+        except Exception, e:
             return False
             print e
 
-    def withdraw(self,amount,bank_id,surplus=1,act="act_account"):
+    def withdraw(self, amount, bank_id, surplus=1, act="act_account"):
         """
         :return:
         1000 提现成功
         1001 额度用完
-        1002 重新登陆
+        1010 重新登陆
         1003 http返回200，未知错误
         1004 http返回非200
         1005 七日提现限制
@@ -83,47 +85,71 @@ class Session(object):
         1007 提现金额不是20的整数倍
         1008 支付密码错误
         """
-        params = {"amount":amount,"raply_bank":bank_id,"surplus_type":surplus,"act":act,"p_password":self.trade_password,"raply_type":0}
+        params = {"amount": amount, "raply_bank": bank_id, "surplus_type": surplus, "act": act,
+                  "p_password": self.trade_password, "raply_type": 0}
         try:
             result = self.session.post(self.withdraw_url, data=params, headers=self.headers, timeout=self.timeout)
-            #print result.status_code
-            #print result.headers
+            # print result.status_code
+            # print result.headers
             if result.text.find(u"当日系统限额已用完，请明天提交提现申请！") != -1:
                 self.flag = False
                 print u'当日额度已用完！'
                 return "1001"
             if result.status_code == 200:
-                if result.text.find(u"提交成功，银行处理中") != -1:
-                    print 'withdraw OK!'
+                if u"提交成功，银行处理中" in result.text:
+                    print '%s 提交成功，银行处理中...' % (self.username)
                     return "1000"
-                elif result.text.find(u"立即注册") != -1 and result.text.find(u"使用合作网站账号登录") != -1:
-                    print 'Need login...'
-                    return "1002"
-                elif result.text.find(u"7天内仅允许1次提现，请您耐心等待！") != -1:
-                    print '7days limit...'
+                elif u"立即注册" in result.text and u"使用合作网站账号登录" in result.text:
+                    print '%s session丢失，请重新登陆...' % (self.username)
+                    return "1010"
+                elif u"7天内仅允许1次提现，请您耐心等待！" in result.text:
+                    print '%s 7天内仅允许1次提现，请您耐心等待！' % (self.username)
                     return "1005"
-                elif result.text.find(u"提现红金宝超出每周提现的限额") != -1:
-                    print u"提现超出每周提现的限额"
+                elif u"提现红金宝超出每周提现的限额" in result.text:
+                    print "%s 提现超出每周提现的限额" % (self.username)
                     return "1006"
-                elif result.text.find(u"提现失败，提现金额必须是20的整数倍") != -1:
-                    print 'Amount%20...'
+                elif u"提现失败，提现金额必须是20的整数倍" in result.text:
+                    print '%s 提现失败，提现金额必须是20的整数倍...' % (self.username)
                     return "1007"
-                elif result.text.find(u"支付密码错误，请重新输入") != -1:
-                    print u'支付密码错误，请重新输入'
+                elif u"支付密码错误，请重新输入" in result.text:
+                    print '%s 支付密码错误，请重新输入' % (self.username)
                     return "1008"
                 else:
                     print result.text
+                    print '%s unkown mistakes...' % (self.username)
                     return "1003"
-                    print 'Unkown mistakes...'
             else:
-                print 'WithDraw httpresponse code %s' % (str(result.status_code))
+                print '%s withdraw httpresponse code %s' % (self.username, str(result.status_code))
                 return "1004"
-        except Exception,e:
+        except Exception, e:
             print e
-            return False
+            return "1003"
 
-    def change_password(self,uid,new_password):
-        params = {"uid":uid,"new_password":new_password,"is_mobile":1}
+    def withdraw_hjb(self, amount, bank_id):
+        """
+
+        :return:
+        1000 提现成功
+        1001 提现失败，服务端限制
+        1002 提现失败，抛出异常
+        1010 提现失败，需要登陆
+        """
+        try:
+            params = {"amount": amount, "raply_bank": bank_id, "act": "act_integ", "p_password": self.trade_password}
+            result = self.session.post(self.withdraw_url, data=params, headers=self.headers, timeout=self.timeout)
+            if u"立即注册" in result.text and u"使用合作网站账号登录" in result.text:
+                print "%s 提现红金宝需要登陆..." % (self.username)
+                return "1010"
+            if result.json()["status"] == "error":
+                print "%s %s " % (self.username,result.json()["message"])
+                return "1001"
+            return "1000"
+        except Exception, e:
+            print e
+            return "1002"
+
+    def change_password(self, uid, new_password):
+        params = {"uid": uid, "new_password": new_password, "is_mobile": 1}
         url = "https://www.3658mall.com/user.php?act=act_edit_password_mobile"
         result = self.session.post(url, data=params, headers=self.headers, timeout=self.timeout)
         print result.text
@@ -140,14 +166,15 @@ class Session(object):
         """
         params = {}
         url = "https://www.3658mall.com/day_sign.php?act=just_sign"
-        result = self.session.post(url,data=params,headers=self.headers,timeout=self.timeout)
-        if result.text.find(u"立即注册") != -1 and result.text.find(u"使用合作网站账号登录") != -1:
-            print "Needs login..."
+        result = self.session.post(url, data=params, headers=self.headers, timeout=self.timeout)
+        if u"立即注册" in result.text and u"使用合作网站账号登录" in result.text:
+            print "%s session丢失，请重新登陆..." % (self.username)
             return "1001"
         elif result.json()["error"] == 1 and result.json()["content"] == u"今日已签到，明日再来吧！":
-            print u"今日已签到！"
+            print u"%s 今日已签到！" % (self.username)
             return "1002"
         elif result.json()["error"] == 1:
+            print '%s unkown mistakes...' % (self.username)
             print result.json()["content"]
             return "1003"
         url = "https://www.3658mall.com/day_sign.php"
@@ -155,12 +182,13 @@ class Session(object):
         url = "https://www.3658mall.com/day_sign.php?act=sign"
         result = self.session.post(url, data=params, headers=self.headers, timeout=self.timeout)
         if result.json()["code"] == 99:
-            print "Sign success!"
+            print "%s 签到成功!" % (self.username)
             return "1000"
         else:
+            print '%s unkown mistakes...' % (self.username)
             print result.json()["content"]
             return "1003"
 
+
 if __name__ == "__main__":
     pass
-
--- handler.py
+++ handler.py
@@ -5,37 +5,39 @@ Created on Jan 24, 2018
 @author: guxiwen
 """
 from base import Session
-import threading,time
+import threading, time
 import redis
 
+
 class Handler(threading.Thread):
-    def __init__(self,threadName,username,password,trade_password):
-        super(Handler,self).__init__(name=threadName)
-        self.session = Session(username,password,trade_password)
+    def __init__(self, threadName, username, password, trade_password):
+        super(Handler, self).__init__(name=threadName)
+        self.session = Session(username, password, trade_password)
 
     def run(self):
         count = 1
-        while not self.session.login() and count<10:
+        while not self.session.login() and count < 10:
             time.sleep(5)
             count = count + 1
         if self.session.flag is False:
             print 'login failed...'
         while self.session.flag:
-            if self.session.withdraw(960,117166):
+            if self.session.withdraw(960, 117166):
                 break
             time.sleep(5)
 
+
 class LoginHandler(threading.Thread):
-    def __init__(self,threadName,username,password,trade_password):
-        super(LoginHandler,self).__init__(name=threadName)
+    def __init__(self, threadName, username, password, trade_password):
+        super(LoginHandler, self).__init__(name=threadName)
         self.username = username
-        self.session = Session(username,password,trade_password)
-        self.r_connector = redis.Redis(host="127.0.0.1",port=6379,db=0)
+        self.session = Session(username, password, trade_password)
+        self.r_connector = redis.Redis(host="127.0.0.1", port=6379, db=0)
 
     def run(self):
         while True:
-            keys = self.r_connector.keys(self.username+"*")
-            if len(keys) >5:
+            keys = self.r_connector.keys(self.username + "*")
+            if len(keys) > 5:
                 print 'sleeping'
                 time.sleep(60)
             else:
@@ -44,41 +46,36 @@ class LoginHandler(threading.Thread):
                     self.session.store_cookie()
             time.sleep(30)
 
+
 class WithdrawHandler(threading.Thread):
     def __init__(self, threadName, username, password, trade_password):
-        super(WithdrawHandler,self).__init__(name=threadName)
-        self.session = Session(username,password,trade_password)
+        super(WithdrawHandler, self).__init__(name=threadName)
+        self.session = Session(username, password, trade_password)
         if not self.session.set_cookies():
             print 'Cookie set failed...'
 
     def run(self):
         while True:
             code = self.session.withdraw(200, 117166)
-            if code == "1000" :
-                print 'Withdraw OK!'
+            if code == "1000":
                 break
             elif code == "1001":
-                print 'Lack of Amount...'
                 break
-            elif code == "1002":
-                print '1002'
+            elif code == "1010":
                 self.session.set_cookies()
                 time.sleep(10)
             elif code == "1004":
                 print '1004'
                 continue
             elif code == "1005":
-                print "7days limit..."
                 break
             elif code == "1006":
-                print u"个人周提现额度限制"
                 break
             elif code == "1008":
-                print u"支付密码错误，请重新输入"
                 break
             else:
-                print "Unkown mistake code[%s]..." % (code)
                 break
+
 
 if __name__ == "__main__":
     pass
